61059670764750d489b16919c87ebb1c50c98b01
Devin-Jackson314 <you@example.com>
2022-09-10 17:07:53 -0400

    trying to get my movies to the page

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GetMediaService } from 'services/get-media.service';
 
 @Component({
   selector: 'app-movies',
@@ -8,9 +8,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private mediaService: GetMediaService) { }
+
+  image: any;
+  image2: any;
+  images: any;
+  mediaOptions: any;
+  thumbnail: any;
+  mediachoice: any;
+  small: any;
+  mediachoice2: any;
+  small2: any;
+  regular: any;
 
   ngOnInit(): void {
+    this.GetMedia();
+
+  }
+
+  GetMedia() {
+
+   
+    return this.mediaService.getAllMedia().subscribe((data) => {
+      
+      this.mediaOptions = data;
+      console.log(this.mediaOptions, "this is all data");
+      console.log(this.mediaOptions[0].thumbnail, "data inside targeted thumbnail");
+      this.mediachoice = this.mediaOptions[0].thumbnail;
+      console.log(this.mediachoice.trending);
+      this.small = this.mediachoice.trending.small;
+      console.log(this.small, "the targeted image");
+
+    
+
+      
+    
+
+    
+      
+
+     
+         console.log(this.mediaOptions[0].thumbnail.regular.small)
+       
+         this.regular = this.mediaOptions[0].thumbnail.regular.small;
+     
+    })
   }
 
 }
