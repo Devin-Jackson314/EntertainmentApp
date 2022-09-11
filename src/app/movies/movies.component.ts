0afb9f2ee648dfd8368af7bafc72b4f0bae3332d
Devin-Jackson314 <you@example.com>
2022-09-11 13:15:31 -0400

    got the right data for tv series on the page

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -41,54 +41,9 @@ export class MoviesComponent implements OnInit {
       this.mediaOptions = data;
       console.log(this.mediaOptions, "all data");
 
-      // console.log(this.mediaOptions, "this is all data");
-      // //console.log(this.mediaOptions[0].thumbnail, "data inside targeted thumbnail");
-      // this.mediachoice = this.mediaOptions[0].thumbnail;
-      // //console.log(this.mediachoice.trending);
-      // this.small = this.mediachoice.trending.small;
-      // //console.log(this.small, "the targeted image");
-
+     
       
-      // this.image = document.createElement("img");
-      // this.image.setAttribute("src", this.small);
-      // this.image.setAttribute("height", "140px");
-      // this.image.setAttribute("width", "240px");
-      // this.image.setAttribute("alt", "image");
-      // this.image.setAttribute("class", "img-responsive");
-      // document.getElementById("imagebox")?.appendChild(this.image);
-
-      // //console.log(this.mediaOptions[1].thumbnail, "bottom gear thumbnail");
-      // this.mediachoice2 = this.mediaOptions[1].thumbnail;
-      // //console.log(this.mediachoice.trending);
-      // this.small2 = this.mediachoice2.trending.small;
-      // //console.log(this.small2, "the targeted image");
       
-      // this.image2 = document.createElement("img");
-      // this.image2.setAttribute("src", this.small2);
-      // this.image2.setAttribute("height", "148px");
-      // this.image2.setAttribute("width", "240px");
-      // this.image2.setAttribute("alt", "image");
-      // this.image2.setAttribute("class", "img-responsive");
-      // document.getElementById("imagebox")?.appendChild(this.image2);
-
-      // for (const i in data) {
-         //console.log(this.mediaOptions[0].thumbnail.regular.small)
-        //console.log(i,this.mediaOptions[0].thumbnail.regular.small,"second thumbnail reference");
-         //this.regular = this.mediaOptions[0].thumbnail.regular.small;
-         //console.log(this.regular, "variable i saved the data in");
-          
-      //    this.images = document.createElement("img");
-      //    this.images.setAttribute("src", this.regular);
-      //    this.images.setAttribute("height", "110px");
-      //    this.images.setAttribute("width", "164px");
-      //    this.images.setAttribute("alt", "image");
-      //    this.images.setAttribute("class", "img-responsive");
-      //   document.getElementById("imagebox2")?.appendChild(this.images);
-
-      //   //maybe i should do a ng for instead
-
-
-      // }
     })
     
  }
--- src/app/tv-series/tv-series.component.ts
+++ src/app/tv-series/tv-series.component.ts
@@ -9,16 +9,20 @@ import { GetMediaService } from 'services/get-media.service';
 export class TvSeriesComponent implements OnInit {
 
   constructor(private getmedia: GetMediaService) { }
-
+  mediaOptions: any;
   ngOnInit(): void {
+    this.GetMedia();
    
   }
 
-  // getMedia() {
-  //   return this.mediaService.getAllMedia().subscribe((data){
-  //     this
-  //   }) 
+  GetMedia() {
+    return this.getmedia.getAllMedia().subscribe((data) => {
+      
+      this.mediaOptions = data;
+      console.log(this.mediaOptions, "all data");
+    })
   
   
 
+  }
 }
