4640ecdb8efa3b1c92f428d0f8eabfdc5452c38b
Devin-Jackson314 <you@example.com>
2022-09-11 16:51:34 -0400

    rewriting my isTrending functionality

--- src/app/trending/trending.component.ts
+++ src/app/trending/trending.component.ts
@@ -41,39 +41,39 @@ export class TrendingComponent implements OnInit {
       
       this.mediaOptions = data;
       console.log(this.mediaOptions, "this is all data");
-      console.log(this.mediaOptions[0].thumbnail, "data inside targeted thumbnail");
-      this.mediachoice = this.mediaOptions[0].thumbnail;
-      console.log(this.mediachoice.trending);
-      this.small = this.mediachoice.trending.small;
-      console.log(this.small, "the targeted image");
+      // console.log(this.mediaOptions[0].thumbnail, "data inside targeted thumbnail");
+      // this.mediachoice = this.mediaOptions[0].thumbnail;
+      // console.log(this.mediachoice.trending);
+      // this.small = this.mediachoice.trending.small;
+      // console.log(this.small, "the targeted image");
 
       
-      this.image = document.createElement("img");
-      this.image.setAttribute("src", this.small);
-      this.image.setAttribute("height", "140px");
-      this.image.setAttribute("width", "240px");
-      this.image.setAttribute("alt", "image");
-      this.image.setAttribute("class", "img-responsive");
-      document.getElementById("imagebox")?.appendChild(this.image);
+      // this.image = document.createElement("img");
+      // this.image.setAttribute("src", this.small);
+      // this.image.setAttribute("height", "140px");
+      // this.image.setAttribute("width", "240px");
+      // this.image.setAttribute("alt", "image");
+      // this.image.setAttribute("class", "img-responsive");
+      // document.getElementById("imagebox")?.appendChild(this.image);
 
-      console.log(this.mediaOptions[1].thumbnail, "bottom gear thumbnail");
-      this.mediachoice2 = this.mediaOptions[1].thumbnail;
-      console.log(this.mediachoice.trending);
-      this.small2 = this.mediachoice2.trending.small;
-      console.log(this.small2, "the targeted image");
+      // console.log(this.mediaOptions[1].thumbnail, "bottom gear thumbnail");
+      // this.mediachoice2 = this.mediaOptions[1].thumbnail;
+      // console.log(this.mediachoice.trending);
+      // this.small2 = this.mediachoice2.trending.small;
+      // console.log(this.small2, "the targeted image");
       
-      this.image2 = document.createElement("img");
-      this.image2.setAttribute("src", this.small2);
-      this.image2.setAttribute("height", "148px");
-      this.image2.setAttribute("width", "240px");
-      this.image2.setAttribute("alt", "image");
-      this.image2.setAttribute("class", "img-responsive");
-      document.getElementById("imagebox")?.appendChild(this.image2);
+      // this.image2 = document.createElement("img");
+      // this.image2.setAttribute("src", this.small2);
+      // this.image2.setAttribute("height", "148px");
+      // this.image2.setAttribute("width", "240px");
+      // this.image2.setAttribute("alt", "image");
+      // this.image2.setAttribute("class", "img-responsive");
+      // document.getElementById("imagebox")?.appendChild(this.image2);
 
       // for (const i in data) {
-         console.log(this.mediaOptions[0].thumbnail.regular.small)
+         //console.log(this.mediaOptions[0].thumbnail.regular.small)
         //console.log(i,this.mediaOptions[0].thumbnail.regular.small,"second thumbnail reference");
-         this.regular = this.mediaOptions[0].thumbnail.regular.small;
+         //this.regular = this.mediaOptions[0].thumbnail.regular.small;
          //console.log(this.regular, "variable i saved the data in");
           
       //    this.images = document.createElement("img");
