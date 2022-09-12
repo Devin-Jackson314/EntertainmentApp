d1ef58e82c08216e6f06dc2ae59fb41bb57cc577
Devin-Jackson314 <you@example.com>
2022-09-12 11:25:37 -0400

    search pipe is now working

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -5,8 +5,28 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any, filterString: string, propName: string): any {
+    let resultArray = [];
+    if (value.length === 0 || filterString ==='') {
+      return value;
+    }
+    for (const item of value) {
+      let filterItem = item[propName].toLowerCase();
+      let theString = filterString.toLowerCase();
+      
+      //console.log(filterItem);
+      //console.log(theString);
+      if (filterItem.includes(theString)) {
+        resultArray.push(item);
+      }
+      
+    }
+      return resultArray;
+    
+    }
   }
 
-}
+//| search: searchFilter: 'TV Series'
+//  if (item[propName] === filterString) {
+//         resultArray.push(item);
+//       }
